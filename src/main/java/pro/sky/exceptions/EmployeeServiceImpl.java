6bbd9227324565bd75c9746103f14c533a08dd5e
AnnaSergeenkova <lutekAnn@yandex.ru>
2021-12-07 21:02:31 +0300

    дз правки final

--- src/main/java/pro/sky/exceptions/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/exceptions/EmployeeServiceImpl.java
@@ -13,6 +13,7 @@ public class EmployeeServiceImpl implements EmployeeService{
             for (int i = 0; i < employees.length; i++) {
                 if (employees[i] == null) {
                     employees[i] = new Employee (firstName, lastName);
+                    return employees[i];
                 }
             }
         throw new OverFlowEmployeeExceptions();
@@ -21,8 +22,12 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public Employee removeEmployee(String firstName, String lastName) {
         for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == new Employee (firstName, lastName)) {
-                employees[i] = null;
+            if (employees[i] != null && employees[i].equals(new Employee (firstName, lastName)))
+             {
+                 Employee employee = employees[i];
+                 employees[i] = null;
+
+                return employee;
             }
         }
         throw new NotFoundEmployeeException();
@@ -31,7 +36,9 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public Employee findEmployee(String firstName, String lastName) {
         for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == new Employee (firstName, lastName)) {
+            if (employees[i] != null && employees[i].equals(new Employee (firstName, lastName)))
+            {
+                return employees[i];
             }
         }
         throw new NotFoundEmployeeException();
